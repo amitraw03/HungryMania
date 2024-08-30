6bc70aab8e69168cd2df0be876abcf7e4ace8265
amitraw03 <99679149+amitraw03@users.noreply.github.com>
2024-08-30 16:33:43 +0530

    shimmer done

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,5 +1,5 @@
 import RestaurantCard from "./RestaurantCard";  //importing default exports
-import { useEffect, useState } from "react";  //importing usestate hook is similr importing named exports
+import { useEffect, useState } from "react";  //importing usestate hook is similr importing named exportsim
 import Shimmer from "./shimmer";   //import fake UI for the 1st rendering
 import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus";
--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -1,13 +1,15 @@
 
 const Shimmer = () => {
     return(
-        <div className="shimmer-container">
-            <div className="shimmer-cards"></div>
-            <div className="shimmer-cards"></div>
-            <div className="shimmer-cards"></div>
-            <div className="shimmer-cards"></div>
-            <div className="shimmer-cards"></div>
+        <>
+        <div className="bg-slate-300 w-[1420px] h-[380px] mt-16 mb-8 mx-auto rounded-lg"></div>
+        <div className="flex flex-wrap ml-20">
+            <div className="bg-slate-300 w-72 h-[520px] px-6 ml-9 mr-3 mb-8 mt-5 rounded-lg "></div>
+            <div className="bg-slate-300 w-72 h-[520px] px-6 ml-9 mr-3 mb-8 mt-5 rounded-lg "></div>
+            <div className="bg-slate-300 w-72 h-[520px] px-6 ml-9 mr-3 mb-8 mt-5 rounded-lg "></div>
+            <div className="bg-slate-300 w-72 h-[520px] px-6 ml-9 mr-3 mb-8 mt-5 rounded-lg "></div>
         </div>
+        </>
     );
 };
 
